6cbef5b84f330ee2acc5e94c7d6bc6f594b81188
yma50 <yma50@sfu.ca>
2019-10-21 19:54:48 -0700

    load json from view.py to template

--- src/home/views.py
+++ src/home/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
+import json
 
 # Create your views here.
-def home_view(request, *args, **kwargs):
+def home_view(request):
     return render(request, "home.html", {"title": "Home"})
 
 
-def pixi_view(request, *args, **kwargs):
-    return render(request, "pixi.html", {"title": "PIXI"})
+def pixi_view(request):
+    json_data = open('./static/images/sprite-game.json')
+    data1 = json.load(json_data)
+    data2 = json.dumps(data1)
+    json_data.close()
+    return render(request, "pixi.html", {"title": "PIXI", "json": data2})
